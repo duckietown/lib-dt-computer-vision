Fixed ground corners computation for the robot reference frame

src/dt_computer_vision_tests/extrinsics_calibration_tests/test_calibration.py
@@ -3,7 +3,7 @@ import os
 import cv2
 import numpy as np
 
-from dt_computer_vision.camera.calibration.extrinsics.boards import CalibrationBoard8by6
+from dt_computer_vision.camera.calibration.extrinsics.boards import CalibrationBoard8by6, ReferenceFrame
 from dt_computer_vision.camera.calibration.extrinsics.chessboard import find_corners
 from dt_computer_vision.camera.calibration.extrinsics.ransac import estimate_homography
 from dt_computer_vision.camera.types import CameraModel
@@ -32,7 +32,29 @@ expected_pp = [0.4, 0]
 board = CalibrationBoard8by6
 camera = CameraModel(**test_camera)
 
+def test_calibration_board_corners():
+    board = CalibrationBoard8by6
+    corners = board.corners(reference_frame=ReferenceFrame.ROBOT)
+    top_left = corners[0]
+    bottom_right = corners[-1]
+    
+    # Check that the top left corner has positive x and y coordinates
+    assert top_left.x >= 0, f"Expected x >= 0, got {top_left.x}."
+    assert top_left.y >= 0, f"Expected y >= 0, got {top_left.y}."
 
+    # Check that the bottom right corner has positive x and negative y coordinates
+    assert bottom_right.x >= 0, f"Expected x >= 0, got {bottom_right.x}."
+    assert bottom_right.y <= 0, f"Expected y >= 0, got {bottom_right.y}."
+    
+    # Check that the top left corner has x coordinate board.square_size*number of rows + x offset
+    # and y coordinate board.y_offset-board.square_size
+    assert top_left.x == board.square_size * board.rows + board.x_offset, f"Expected x == {board.square_size * board.rows + board.x_offset}, got {top_left.x}."
+    assert top_left.y == board.y_offset - board.square_size, f"Expected y == {board.y_offset - board.square_size}, got {top_left.y}."
+    
+    # Check that the bottom right corner has x coordinate x offset and y coordinate y_offset - board.square_size*(number of columns+1)
+    assert bottom_right.x == board.x_offset + board.square_size, f"Expected x == {board.x_offset + board.square_size}, got {bottom_right.x}."
+    assert bottom_right.y == board.y_offset - board.square_size * board.columns, f"Expected y == {board.y_offset - board.square_size * (board.columns)}, got {bottom_right.y}."
+    
 def test_extrinsics_calibration_image1():
     image1_fpath: str = os.path.join(assets_dir, "image1.jpg")
     image1 = cv2.imread(image1_fpath)
@@ -41,9 +63,9 @@ def test_extrinsics_calibration_image1():
     # find corners
     corners = find_corners(image1, board)
     print(f"Found {len(corners)} corners.")
-    assert len(corners) == (board.columns - 1) * (board.rows - 1)
+    assert len(corners) == board.columns * board.rows, f"Expected {len(corners)} corners."
     # estimate homography
-    H = estimate_homography(corners, board, camera)
+    H = estimate_homography(corners, board, camera, ref_frame=ReferenceFrame.ROBOT)
     # project the principal point onto the plane
     ground_pp = np.dot(H, [0, 0, 1])
     ground_pp = (ground_pp / ground_pp[2])[:2]
@@ -51,4 +73,7 @@ def test_extrinsics_calibration_image1():
     error = np.linalg.norm(ground_pp - expected_pp)
     print(f"Error is ~{error * 100:.2f}cm.")
     # make sure the error is within 3cm
-    assert error <= 0.03
+    assert error <= 0.03, f"Error is {error * 100:.2f}cm, expected less than 3cm."
+
+if __name__ == "__main__":
+    test_extrinsics_calibration_image1()